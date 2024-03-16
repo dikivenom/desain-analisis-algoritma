20ca087ef2f99a6819699d17efeae48f3b7b0a83
Dicky Ramadhan <80454607+dikivenom@users.noreply.github.com>
2024-03-16 10:12:20 +0700

    Update tugas4_perpustakaan.cpp

--- Pertemuan 1/tugas4_perpustakaan.cpp
+++ Pertemuan 1/tugas4_perpustakaan.cpp
@@ -2,42 +2,41 @@
 using namespace std;
 
 int main() {
-    const int MAX_BUKU = 100; 
-    string daftarBuku[MAX_BUKU]; 
-    int jumlahBuku; 
+    const int maxArrayBuku = 100; 
+    string listBuku[maxArrayBuku]; 
+    int jmlBuku; 
 
     
     cout << "Masukkan jumlah buku: ";
-    cin >> jumlahBuku;
+    cin >> jmlBuku;
 
-    //  menampilkan judul buku
+   
     cout << "Masukkan dan tampilkan judul buku:" << endl;
-    for (int i = 0; i < jumlahBuku; i++) {
+    for (int i = 0; i < jmlBuku; i++) {
         cout << "Judul buku ke-" << i + 1 << ": ";
-        cin >> daftarBuku[i];
-        cout << daftarBuku[i] << endl;
+        cin >> listBuku[i];
+        cout << listBuku[i] << endl;
     }
 
     // Melakukan selection sort
-    for (int i = 0; i < jumlahBuku - 1; i++) {
-        int minIndex = i;
-        for (int j = i + 1; j < jumlahBuku; j++) {
-            if (daftarBuku[j] < daftarBuku[minIndex]) {
-                minIndex = j;
+    for (int i = 0; i < jmlBuku - 1; i++) {
+        int hurufArray = i; 
+        for (int j = i + 1; j < jmlBuku; j++) {
+            if (listBuku[j] < listBuku[hurufArray]) {
+                hurufArray = j;
             }
         }
-        // Tukar judul buku pada indeks i dengan judul buku pada indeks minIndex
-        string temp = daftarBuku[i];
-        daftarBuku[i] = daftarBuku[minIndex];
-        daftarBuku[minIndex] = temp;
+     //Penukaran
+        string temp = listBuku[i];
+        listBuku[i] = listBuku[hurufArray]; 
+        listBuku[hurufArray] = temp;
     }
 
     // Menampilkan daftar buku setelah diurutkan 
     cout << "\nDaftar buku setelah diurutkan:" << endl;
-    for (int i = 0; i < jumlahBuku; i++) {
-        cout << daftarBuku[i] << endl;
+    for (int i = 0; i < jmlBuku; i++) {
+        cout << listBuku[i] << endl;
     }
 
     return 0;
 }
-
